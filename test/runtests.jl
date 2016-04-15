3d5b0bc0bcd6729c521175248760c67a391ba922
Simon Byrne <simonbyrne@gmail.com>
2016-04-15 12:53:57 -0400

    change tests to check OS

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
 using WinReg
 using Base.Test
 
-@test querykey(WinReg.HKEY_LOCAL_MACHINE,"System\\CurrentControlSet\\Control\\Session Manager\\Environment","PROCESSOR_ARCHITECTURE") == ENV["PROCESSOR_ARCHITECTURE"]
+@test querykey(WinReg.HKEY_LOCAL_MACHINE,"System\\CurrentControlSet\\Control\\Session Manager\\Environment","OS") == "Windows_NT"
