346b0a4018673971d1e75977eeb9df54159dbe74
Leonardo <maxsax74@gmail.com>
2016-11-23 23:31:52 +0100

    fixed method docs and failure behavior

--- src/regfunc.jl
+++ src/regfunc.jl
@@ -2,17 +2,14 @@
 #-----------------------------------------------------------
 # Define function to access windows registry
 # -- part of WinReg Module --
-#
-#File: regutils.jl
-#Author: Leonardo Cocco
-#Creation Date: 11-04-2016
-#Last update 21-11-2016
-#ver: 0.11
 #----------------------------------------------------------
 #----------------------------------------------------------
 
 
-"""Read a value with `valuename` into `key`. 
+"""
+   regread(key::AbstractString, valuename::AbstractString)
+
+Read a value with `valuename` into `key`. 
 `key` can be specified using abbreviated form (e.g. 'HKEY_LOCAL_MACHINE' -> 'HKLM').
 Return type in Julia is based onto value type stored in registry; returns `nothing` (type is `Void`) if key not found"""
 function regread(key::AbstractString, valuename::AbstractString)
@@ -25,21 +22,21 @@ function regread(key::AbstractString, valuename::AbstractString)
 	local len = Ref{DWORD}(0)
 	local typ = Ref{DWORD}(0)
 	
-	hk = regopenkeyex(root, subkey, REG_SAM.KEY_READ)
-	if hk != 0
+	try
+		hk = regopenkeyex(root, subkey, REG_SAM.KEY_READ)
 		#get buffer dim
-		local res = ccall((:RegQueryValueExW, advapi), stdcall,
+		res = ccall((:RegQueryValueExW, advapi), stdcall,
 		  LONG,
-		  (HKEY, LPCWSTR, LPDWORD, LPDWORD, LPBYTE, LPDWORD),
-		  hk, transcode(Cwchar_t, valuename), C_NULL, typ, C_NULL, len )
+		  (HKEY, Cwstring, LPDWORD, LPDWORD, LPBYTE, LPDWORD),
+		  hk, valuename, C_NULL, typ, C_NULL, len )
 		if res == ERROR_SUCCESS
 			buf = Vector{UInt8}(len[])
 			len = Ref{DWORD}(length(buf))
 			
 			res = ccall((:RegQueryValueExW, advapi), stdcall,
 			  LONG,
-			  (HKEY, LPCWSTR, LPDWORD, LPDWORD, LPBYTE, LPDWORD),
-			  hk, transcode(Cwchar_t, valuename), C_NULL, typ, pointer(buf), len )
+			  (HKEY, Cwstring, LPDWORD, LPDWORD, LPBYTE, LPDWORD),
+			  hk, valuename, C_NULL, typ, pointer(buf), len )
 			
 			regclosekey(hk)
 			
@@ -49,18 +46,30 @@ function regread(key::AbstractString, valuename::AbstractString)
 		else
 			regclosekey(hk)
 			
-			return nothing
+			error("error getting value length")
 		end
-	else
+	finally
+		regclosekey(hk)
+		
 		return nothing
 	end
 	
 	if typ[] == REG_TYPE.REG_SZ || typ[] == REG_TYPE.REG_EXPAND_SZ || typ[] == REG_TYPE.REG_LINK
 		local wbuf = reinterpret(Cwchar_t, buf)
-		return transcode(String, wbuf[1:div((len[]-2),2)])
+		l = len[]
+		if buf[l] == 0 and buf[l-1] == 0
+			return transcode(String, wbuf[1:div(l-2,2)])
+		else
+			return transcode(String, wbuf[1:div(l,2)])
+		end
 	elseif typ[] == REG_TYPE.REG_MULTI_SZ
 		wbuf = reinterpret(Cwchar_t, buf)
-		s = transcode(String, wbuf[1:div((len[]-2),2)])
+		l = len[]
+		if buf[l] == 0 and buf[l-1] == 0
+			s = transcode(String, wbuf[1:div(l-2,2)])
+		else
+			s = transcode(String, wbuf[1:div(l,2)])
+		end
 		v = split(s, '\0')
 		return v[1:(end-1)]
 	elseif typ[] == REG_TYPE.REG_DWORD_LITTLE_ENDIAN
@@ -76,7 +85,10 @@ function regread(key::AbstractString, valuename::AbstractString)
 	end
 end
 
-"""Delete specified `key`. 
+"""
+   regdelete(key::AbstractString, view64::Bool = false)
+
+Delete specified `key`. 
 `key` can be specified using abbreviated form (e.g. 'HKEY_LOCAL_MACHINE' -> 'HKLM').  
 `view64` specifies if key is 64 or 32 bit view (default); see https://msdn.microsoft.com/en-us/library/aa384129.aspx   
 Cannot delete key with subkeys.  
@@ -96,8 +108,8 @@ function regdelete(key::AbstractString, view64::Bool = false)
 	else
 		local res = ccall((:RegDeleteKeyExW, advapi), stdcall,
 		  LONG,
-		  (HKEY, LPCWSTR, REGSAM, DWORD),
-		  hk, transcode(Cwchar_t ,delkey), (view64 ? REG_SAM.KEY_WOW64_64KEY : REG_SAM.KEY_WOW64_32KEY), 0 )
+		  (HKEY, Cwstring, REGSAM, DWORD),
+		  hk, delkey, (view64 ? REG_SAM.KEY_WOW64_64KEY : REG_SAM.KEY_WOW64_32KEY), 0 )
 		
 		regclosekey(hk)
 		
@@ -105,7 +117,10 @@ function regdelete(key::AbstractString, view64::Bool = false)
 	end
 end
 
-"""Delete specified `valuename` under specified `key`.  
+"""
+   regdelete(key::AbstractString, valuename::AbstractString)
+
+Delete specified `valuename` under specified `key`.  
 `key` can be specified using abbreviated form (e.g. 'HKEY_LOCAL_MACHINE' -> 'HKLM').  
 If operation has success is returned `true`, `false` otherwise"""
 function regdelete(key::AbstractString, valuename::AbstractString)
@@ -121,8 +136,8 @@ function regdelete(key::AbstractString, valuename::AbstractString)
 	else
 		local res = ccall((:RegDeleteValueW, advapi), stdcall,
 		  LONG,
-		  (HKEY, LPCWSTR),
-		  hk, transcode(Cwchar_t, valuename) )
+		  (HKEY, Cwstring),
+		  hk, valuename )
 		
 		regclosekey(hk)
 		
@@ -130,51 +145,78 @@ function regdelete(key::AbstractString, valuename::AbstractString)
 	end
 end
 
-"""Write `value` to `valuename` under specified `key`.  
+
+function preparedata(value::AbstractString)
+	typ = REG_TYPE.REG_SZ
+	data = string2wchar(value)
+	
+	return typ, data
+nd
+
+function preparedata(value::Vector{String})
+	typ = REG_TYPE.REG_MULTI_SZ
+	len = 0
+	for idx in 1:length(value)
+		len += length(value[idx])
+	end
+	data = Vector{UInt8}(len*2+length(value)*2+2)
+	local pos = 1
+	for idx in 1:length(value)
+		dt = string2wchar(value[idx])
+		copy!(data, pos, dt, 1, length(dt))
+		pos += length(dt)
+	end
+	data[end] = 0
+	
+	return typ, data
+end
+
+function preparedata(value::Int32)
+	typ = REG_TYPE.REG_DWORD
+	buf = IOBuffer()
+	write(buf, htol(value))
+	data = buf.data
+	
+	return typ, data
+end
+
+function preparedata(value::UInt32)
+	return preparedata(Int32(value))
+end
+
+function preparedata(value::Int64)
+	typ = REG_TYPE.REG_DWORD
+	buf = IOBuffer()
+	write(buf, htol(value))
+	data = buf.data
+	
+	return typ, data
+end
+
+function preparedata(value::UInt64)
+	return preparedata(Int64(value))
+end
+
+function preparedata(value::Vector{UInt8})
+	typ = REG_TYPE.REG_BINARY
+	data = value
+	
+	return typ, data
+end
+
+"""
+   regwrite(key::AbstractString, valuename::AbstractString, value::Any)
+
+Write `value` to `valuename` under specified `key`.  
 `key` can be specified using abbreviated form (e.g. 'HKEY_LOCAL_MACHINE' -> 'HKLM').  
 Return `true` if successful, `false` otherwise"""
 function regwrite(key::AbstractString, valuename::AbstractString, value::Any)
-	local root = getroothkey(key)
+	root = getroothkey(key)
 	if root == 0
 		return false
 	end
-	local subkey = getsubkey(key)
-	
-	local typ::DWORD = 0
-	local data::Vector{UInt8} = Vector{UInt8}()
-	if typeof(value) <: AbstractString
-		typ = REG_TYPE.REG_SZ
-		data = string2wchar(value)
-	elseif typeof(value) <: Vector && eltype(value) <: AbstractString
-		typ = REG_TYPE.REG_MULTI_SZ
-		local len = 0
-		for idx in 1:length(value)
-			len += length(value[idx])
-		end
-		data = Vector{UInt8}(len*2+length(value)*2+2)
-		local pos = 1
-		for idx in 1:length(value)
-			dt = string2wchar(value[idx])
-			copy!(data, pos, dt, 1, length(dt))
-			pos += length(dt)
-		end
-		data[end] = 0
-	elseif typeof(value) <: UInt32 || typeof(value) <: Int32
-		typ = REG_TYPE.REG_DWORD
-		buf = IOBuffer()
-		write(buf, htol(value))
-		data = buf.data
-	elseif typeof(value) <: UInt64 || typeof(value) <: Int64
-		typ = REG_TYPE.REG_QWORD
-		buf = IOBuffer()
-		write(buf, htol(value))
-		data = buf.data
-	elseif typeof(value) <: Vector{UInt8}
-		typ = REG_TYPE.REG_BINARY
-		data = value
-	else
-		return false
-	end
+	subkey = getsubkey(key)
+	typ, data = preparedata(value)
 	
 	local hk = regopenkeyex(root, subkey, REG_SAM.KEY_WRITE)
 	if hk == 0
@@ -182,13 +224,16 @@ function regwrite(key::AbstractString, valuename::AbstractString, value::Any)
 	end
 	local res = ccall((:RegSetKeyValueW, advapi), stdcall,
 	  LONG,
-	  (HKEY, LPCWSTR, LPCWSTR, DWORD, LPBYTE, DWORD ),
-	  hk, C_NULL, transcode(Cwchar_t, valuename), typ, pointer(data), length(data) )
+	  (HKEY, Cwstring, LPCWSTR, DWORD, LPBYTE, DWORD ),
+	  hk, C_NULL, valuename, typ, pointer(data), length(data) )
 	
 	return (res == ERROR_SUCCESS)
 end
 
-"""Recursively create specified key. 
+"""
+   regcreatekey(key::AbstractString)
+
+Recursively create specified key. 
 `key` can be specified using abbreviated form (e.g. 'HKEY_LOCAL_MACHINE' -> 'HKLM').  
 Return 0 if key is existent, 1 if successful, -1 otherwise"""
 function regcreatekey(key::AbstractString)
@@ -229,8 +274,8 @@ function regcreatekey(key::AbstractString)
 		subreg = String(regpath[i])
 		res = ccall((:RegCreateKeyExW, advapi), stdcall,
 		  LONG,
-		  (HKEY, LPCWSTR, DWORD, LPCWSTR, DWORD, REGSAM, LPSECURITY_ATTRIBUTES, PHKEY, LPDWORD),
-		  hk, transcode(Cwchar_t, subreg), 0, C_NULL, REG_OPTION.NON_VOLATILE, REG_SAM.KEY_WRITE, C_NULL, newhk, C_NULL )
+		  (HKEY, Cwstring, DWORD, LPCWSTR, DWORD, REGSAM, LPSECURITY_ATTRIBUTES, PHKEY, LPDWORD),
+		  hk, subreg, 0, C_NULL, REG_OPTION.NON_VOLATILE, REG_SAM.KEY_WRITE, C_NULL, newhk, C_NULL )
 		if res != ERROR_SUCCESS
 			regclosekey(hk)
 			return -1
@@ -242,7 +287,10 @@ function regcreatekey(key::AbstractString)
 	return 1
 end
 
-"""Return all sukbeys of specified `key` as string array. 
+"""
+   regkeylist(key::AbstractString)
+
+Return all sukbeys of specified `key` as string array. 
 `key` can be specified using abbreviated form (e.g. 'HKEY_LOCAL_MACHINE' -> 'HKLM').  
 If operation fails is returned `nothing` (type is `Void`)"""
 function regkeylist(key::AbstractString)
@@ -294,7 +342,10 @@ function regkeylist(key::AbstractString)
 	return keys
 end
 
-"""Return all value names of specified `key` as tuple array (name, Julia-type).  
+"""
+   regvaluelist(key::AbstractString)
+
+Return all value names of specified `key` as tuple array (name, Julia-type).  
 `key` can be specified using abbreviated form (e.g. 'HKEY_LOCAL_MACHINE' -> 'HKLM').  
 Return array of tuple (<valuename>, <vlauetype>); otherwise `nothing` is returned (type is `Void`)"""
 function regvaluelist(key::AbstractString)
