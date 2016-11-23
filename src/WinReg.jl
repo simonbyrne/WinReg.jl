4ae13f3ae51f090585883261ebe320d380e854e7
Leonardo <maxsax74@gmail.com>
2016-11-23 23:28:39 +0100

    fixed some stilistic details (header, tab vs sp, etc.)

--- src/WinReg.jl
+++ src/WinReg.jl
@@ -1,17 +1,16 @@
 #-----------------------------------------------------------
 #-----------------------------------------------------------
 # Define function to access windows registry
-#
-#File: jWinReg.jl
-#Author: Leonardo Cocco
-#Creation Date: 11-04-2016
-#Last update 21-11-2016
-#ver: 0.12
 #----------------------------------------------------------
 #----------------------------------------------------------
 
 __precompile__(true)
 module WinReg
+
+#Julia 0.4.x compatibility
+if VERSION < v"0.5.0"
+	include_string("is_windows() = (@windows ? true : false)")
+end
 
 if is_windows()
 	include("wintypes.jl")
@@ -19,11 +18,11 @@ if is_windows()
 	
 	include("regfunc.jl")
 	export regread,
-		   regwrite,
-		   regcreatekey,
-		   regdelete,
-		   regkeylist,
-		   regvaluelist
+	       regwrite,
+	       regcreatekey,
+	       regdelete,
+	       regkeylist,
+	       regvaluelist
 	
 	include("deprecations.jl")
 	export querykey
