c7980efb5f4bff78ed44b2548c65aaa02a0419d1
Simon Byrne <simonbyrne@gmail.com>
2023-03-13 11:24:03 -0700

    add more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,20 +1,26 @@
 using WinReg
 using Test
 
-@show collect(WinReg.subkeys(WinReg.openkey(WinReg.HKEY_CURRENT_USER, "Software")))
-
 
 regkey = WinReg.openkey(WinReg.HKEY_LOCAL_MACHINE,"System\\CurrentControlSet\\Control\\Session Manager")
+@test sprint(show, regkey) isa String
 
 @test "Environment" in WinReg.subkeys(regkey)
-
-@show regkey
-@show WinReg.RegKey()
-
-names = collect(WinReg.subkeys(regkey))
-@test "Environment" in names
+@test "Environment" in collect(WinReg.subkeys(regkey))
 
 subkey = WinReg.openkey(regkey, "Environment")
+@test sprint(show, subkey) isa String
+
+@test haskey(subkey, "OS")
+@test "OS" in keys(subkey)
 @test subkey["OS"] == "Windows_NT"
+@test get(subkey, "OS", nothing) == "Windows_NT"
+
+@test !haskey(subkey, "XOS")
+@test !("XOS" in keys(subkey))
+@test isnothing(get(subkey, "XOS", nothing))
+
+
+@test sprint(show, WinReg.RegKey()) == "WinReg.RegKey()"
 
 @test querykey(WinReg.HKEY_LOCAL_MACHINE,"System\\CurrentControlSet\\Control\\Session Manager\\Environment","OS") == "Windows_NT"
